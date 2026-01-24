2e9332d8710cfcd583e511517d4bcf60ff159797
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-24 20:06:42 +0300

    feat: переопределение методов toString(), equals(), hashCode(), compareTo().

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -1,11 +1,16 @@
 package org.example;
 
-public class City {
+import java.util.Objects;
+
+public class City implements Comparable<City> {
     protected String name;
     protected int population;
     protected int year;
 
-    protected City() {}
+    private volatile int hashCode;
+
+    protected City() {
+    }
 
 /*
     public City(String name, int population, int year) {
@@ -45,5 +50,37 @@ public class City {
     }
 */
 
+    @Override
+    public String toString() {
+        return "City name: " + name + "\n" +
+                "Population: " + population + "\n" +
+                "Year of foundation: " + year + "\n";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof City secondCity)) return false;
+
+        return population == secondCity.population
+                && year == secondCity.year
+                && name.equalsIgnoreCase(secondCity.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = hashCode;
+        if (result == 0) {
+            result = Objects.hash(population, year);
+            result = result*31 + name.hashCode();
+            hashCode = result;
+        }
+        return result;
+    }
+
+    @Override
+    public int compareTo(City city) {
+        return (name == city.name) ? 0 : name.compareTo(city.name);
+    }
 }
 
