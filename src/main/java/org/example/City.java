ad3ae810c12a1b1b02b17d47fbcc290ad0680a77
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-24 21:00:46 +0300

    feat: добавление set методов (после обсуждения).

--- src/main/java/org/example/City.java
+++ src/main/java/org/example/City.java
@@ -3,52 +3,38 @@ package org.example;
 import java.util.Objects;
 
 public class City implements Comparable<City> {
-    protected String name;
-    protected int population;
-    protected int year;
+    String name;
+    int population;
+    int year;
 
     private volatile int hashCode;
 
     protected City() {
     }
 
-/*
-    public City(String name, int population, int year) {
-        this.name = name;
-        this.population = population;
-        this.year = year;
-    }
-*/
-
     public String getName() {
         return name;
     }
 
-/*
-    public void setName(String name) {
+    void setName(String name) {
         this.name = name;
     }
-*/
 
     public int getPopulation() {
         return population;
     }
 
-/*
-    public void setPopulation(int population) {
+    void setPopulation(int population) {
         this.population = population;
     }
-*/
 
     public int getYear() {
         return year;
     }
 
-/*
-    public void setYear(int year) {
+    void setYear(int year) {
         this.year = year;
     }
-*/
 
     @Override
     public String toString() {
