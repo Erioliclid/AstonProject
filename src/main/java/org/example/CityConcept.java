2bc019da2e9d1df44a33cc2f228b75357578dd63
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-24 20:08:24 +0300

    feat: концепт реализации интерфейса ICityBuilder.

---
+++ src/main/java/org/example/CityConcept.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Objects;
+
+public class CityConcept implements ICityBuilder {
+    String name;
+    int population;
+    int year;
+
+
+    public CityConcept(String name) {
+        Objects.requireNonNull(name);
+        setName(name);
+    }
+
+    public CityConcept() {
+    }
+
+    @Override
+    public ICityBuilder setName(String name) {
+        Objects.requireNonNull(name);
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public ICityBuilder setPopulation(int population) {
+        this.population = population;
+        return this;
+    }
+
+    @Override
+    public ICityBuilder setYear(int year) {
+        this.year = year;
+        return this;
+    }
+
+    @Override
+    public City getCityAfterBuild() {
+        City city = new City();
+        city.name = this.name;
+        city.population = this.population;
+        city.year = this.year;
+        return city;
+    }
+}
