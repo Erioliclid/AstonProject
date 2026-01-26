9393687d58acb18b024dc99dabe8ae1b577fbbdd
b0sterzer0 <zerbster010@gmail.com>
2026-01-26 10:23:37 +0300

    Added checker line format

--- src/main/java/files/FileReader.java
+++ src/main/java/files/FileReader.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class FileReader {
+    private static final Pattern FORMAT_PATTERN =
+            Pattern.compile("^\\s*[^|]+\\s*\\|\\s*[^|]+\\s*\\|\\s*[^|]+\\s*$");
 
-    private static boolean checkPath(Path path) {
+    private static boolean isValidPath(Path path) {
         // Если по указанному пути действительно что-то существует и это является файлом
         // с расширением .txt вернет true, иначе false
         if (Files.exists(path)
@@ -23,15 +26,31 @@ public class FileReader {
         }
     }
 
+    private static boolean isValidLineFormat(String line) {
+        // Проверка строки, что она не пустая и соответствует паттерну ("Название города | население | год основания")
+        if (line == null || line.trim().isEmpty()) {
+            return false;
+        }
+
+        return FORMAT_PATTERN.matcher(line).matches();
+    }
+
     public static void readFile(Path path) throws IOException {
         // Построчное чтение файла
-        if (checkPath(path)) {
+        if (isValidPath(path)) {
             try (Stream<String> stream = Files.lines(path)) {
                 AtomicLong lineCounter = new AtomicLong(0);
+                AtomicLong errorLineCounter = new AtomicLong(0);
                 stream.forEach(line -> {
                     lineCounter.incrementAndGet();
-                    System.out.println(line);
+                    if (isValidLineFormat(line)) {
+                        System.out.println(line);
+                    }
+                    else errorLineCounter.incrementAndGet();
                 });
+                System.out.println();
+                System.out.println("Всего строк прочитано: " + lineCounter);
+                System.out.println("Выявлено строк, не подходящих по формату: " + errorLineCounter);
             }
         }
     }
