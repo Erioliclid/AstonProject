6d468b50851fc3ff034296319a4bc7dca1aa63b5
Anton Shershan <a.sher.ki.95@gmail.com>
2026-01-27 17:19:32 +0300

    Реализация меню и запуска через мейн

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.appConfig.AppManager;
+
 public class Main {
     public static void main(String[] args) {
+        new Main().startApp();
+    }
 
+    public void startApp(){
+        AppManager appManager = new AppManager();
+        appManager.run();
     }
 }
---
+++ src/main/java/org/example/appConfig/AppState.java
@@ -0,0 +1,4 @@
+package org.example.appConfig;
+
+public class AppState {
+}
