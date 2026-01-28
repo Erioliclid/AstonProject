0a69e19e34d678a6e7af9aac4a22bc1e83eca9cf
Anton Shershan <a.sher.ki.95@gmail.com>
2026-01-28 17:15:57 +0300

    Изменение работы меню с текущим списком + тесты + мавен

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,6 @@ import org.example.appConfig.AppManager;
 
 public class Main {
     public static void main(String[] args) {
-        new Main().startApp();
-    }
-
-    public void startApp(){
         AppManager appManager = new AppManager();
         appManager.run();
     }
