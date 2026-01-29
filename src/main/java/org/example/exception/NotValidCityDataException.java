2b46f69a99d69b55c7b7a1f0e0c4ab7cf1030bc2
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-29 22:51:11 +0300

    refactor: все методы исключения используют реализацию старшего класса.

--- src/main/java/org/example/exception/NotValidCityDataException.java
+++ src/main/java/org/example/exception/NotValidCityDataException.java
@@ -3,19 +3,5 @@ package org.example.exception;
 import java.io.IOException;
 
 public class NotValidCityDataException extends IOException {
-    public NotValidCityDataException(Throwable cause) {
-        super(cause);
-    }
 
-    public NotValidCityDataException() {
-        super();
-    }
-
-    public NotValidCityDataException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotValidCityDataException(String message) {
-        super(message);
-    }
 }
