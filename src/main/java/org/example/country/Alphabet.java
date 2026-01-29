5b803ba64366dbf4d64b99d74e16f248a3858cc1
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-29 19:19:40 +0300

    feat: класс перечисления алфавита для различных стран. Предназначен для генерации названия город по алфавитному признаку. Содержит ключ - двухбуквенный код страны. Поля ключа: регулярное выражение для алфавита, первая строчная буква алфавита, последняя строчная буква алфавита, первая прописная буква алфавита, последняя прописная буква алфавита. И соответствующие методы для получения значения этих полей, а так же общего количества элементов в Enum.

---
+++ src/main/java/org/example/country/Alphabet.java
@@ -0,0 +1,55 @@
+package org.example.country;
+
+public enum Alphabet {
+    EN ("[a-zA-Z]", 'a', 'z', 'A', 'Z'),
+    RU ("[а-яА-Я]", 'а', 'я', 'А', 'Я');
+
+    private final String letters;
+    private final int id;
+    private static int quantity;
+    private final char firstLowercase;
+    private final char lastLowercase;
+    private final char firstCapital;
+    private final char lastCapital;
+
+    private static int addQuantity() {
+        return quantity++;
+    }
+
+    Alphabet(String alphabet, char startLowercase, char endLowercase, char startCapital, char endCapital) {
+        this.letters = alphabet;
+        this.firstLowercase = startLowercase;
+        this.lastLowercase = endLowercase;
+        this.firstCapital = startCapital;
+        this.lastCapital = endCapital;
+        this.id = addQuantity();
+    }
+
+    public String getRegExp() {
+        return this.letters;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static int getQuantity() {
+        return quantity;
+    }
+
+    public int firstLowercase() {
+        return this.firstLowercase;
+    }
+
+    public int lastLowercase() {
+        return this.lastLowercase;
+    }
+
+    public int firstCapital() {
+        return this.firstCapital;
+    }
+
+    public int lastCapital() {
+        return this.lastCapital;
+    }
+}
