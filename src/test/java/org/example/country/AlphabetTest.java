bcbd527949deff85fa271e5bdd68c783e1432201
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-29 19:09:22 +0300

    test: покрытие для класса перечисления Alphabet. Соответствие: регулярных выражений для алфавитов; кол-ва элементов в перечислении; первая строчная буква алфавита; последняя строчная буква алфавита; первая прописная буква алфавита; последняя прописная буква алфавита; содержание элементов класса перечисления; строки-определители элемента перечисления; корректности получения элемента класса перечисления по строковым литералам.

---
+++ src/test/java/org/example/country/AlphabetTest.java
@@ -0,0 +1,83 @@
+package org.example.country;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlphabetTest {
+
+    @Test
+    void getRegExp() {
+        assertEquals("[a-zA-Z]", Alphabet.EN.getRegExp());
+        assertEquals("[а-яА-Я]", Alphabet.RU.getRegExp());
+    }
+
+    @Test
+    void getId() {
+        assertEquals(0, Alphabet.EN.getId());
+        assertEquals(1, Alphabet.RU.getId());
+    }
+
+    @Test
+    void getQuantity() {
+        assertEquals(2, Alphabet.getQuantity());
+    }
+
+    @Test
+    void firstLowercase() {
+        assertEquals('a', Alphabet.EN.firstLowercase());
+        assertEquals('а', Alphabet.RU.firstLowercase());
+    }
+
+    @Test
+    void lastLowercase() {
+        assertEquals('z', Alphabet.EN.lastLowercase());
+        assertEquals('я', Alphabet.RU.lastLowercase());
+    }
+
+    @Test
+    void firstCapital() {
+        assertEquals('A', Alphabet.EN.firstCapital());
+        assertEquals('А', Alphabet.RU.firstCapital());
+    }
+
+    @Test
+    void lastCapital() {
+        assertEquals('Z', Alphabet.EN.lastCapital());
+        assertEquals('Я', Alphabet.RU.lastCapital());
+    }
+
+    @Test
+    void values() {
+        HashSet<Alphabet> allValue = new HashSet<>();
+
+        allValue.add(Alphabet.EN);
+        allValue.add(Alphabet.RU);
+
+        Arrays.stream(Alphabet.values()).toList().forEach(allValue::remove);
+        assertTrue(allValue.isEmpty());
+    }
+
+    @Test
+    void valueOf() {
+        assertSame(Alphabet.EN, Alphabet.valueOf("EN"));
+        assertSame(Alphabet.RU, Alphabet.valueOf("RU"));
+        assertSame(Alphabet.EN, Alphabet.valueOf(Alphabet.class, "EN"));
+        assertSame(Alphabet.RU, Alphabet.valueOf(Alphabet.class, "RU"));
+    }
+
+    @Test
+    void ordinal() {
+        assertEquals(0, Alphabet.EN.ordinal());
+        assertEquals(1, Alphabet.RU.ordinal());
+    }
+
+    @Test
+    void toStringTest() {
+        assertEquals("EN", Alphabet.EN.toString());
+        assertEquals("RU", Alphabet.RU.toString());
+    }
+}
